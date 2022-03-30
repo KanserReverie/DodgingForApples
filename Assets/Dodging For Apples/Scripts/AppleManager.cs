9337ae3fdd89489da4b34a74f2bf8823fba058a3
Kieran <kieranc.inbox@gmail.com>
2022-03-30 21:44:30 +1100

    Apples Are Spawning At Locations

--- Assets/Dodging For Apples/Scripts/AppleManager.cs
+++ Assets/Dodging For Apples/Scripts/AppleManager.cs
@@ -1,21 +1,78 @@
-using System.Collections;
+using Sirenix.OdinInspector;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DodgingForApples
 {
     public class AppleManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField, ReadOnly] private AppleSpawnPoint[] appleSpawnPoints;
+        [SerializeField] private int maxNumberOfApples = 3;
+        [SerializeField, ReadOnly] private int numberOfApplesInScene = 0;
+        [SerializeField] private GameObject applePrefab;
+
+        private void OnValidate()
         {
-        
+            Refresh_appleSpawnPoints();
+            Check_maxNumberOfApples_IsntTooLarge();
         }
 
-        // Update is called once per frame
-        void Update()
+        // Start is called before the first frame update
+        private void Start()
         {
-        
+            Refresh_appleSpawnPoints();
+            Check_maxNumberOfApples_IsntTooLarge();
+            SpawnAllApples();
         }
+
+        private void SpawnAllApples()
+        {
+            for(int i = 0; i < maxNumberOfApples; i++)
+            {
+                   SpawnApple();
+            }
+        }
+
+        private void SpawnApple()
+        {
+            if(applePrefab == null) {
+                 Debug.LogWarning("Sorry, no Apple Prefab to Spawn"); return; }
+            
+            List<int> vacentAppleSpawnPointIndexs = new List<int>();
+            vacentAppleSpawnPointIndexs = CheckVacentSpawnPoints();
+
+            if(vacentAppleSpawnPointIndexs == null) {
+                Debug.LogWarning("Sorry, no vacant Apple Spawn Points."); return; }
+            
+            int randomVacantAppleSpawnPoint = vacentAppleSpawnPointIndexs[Random.Range(0, vacentAppleSpawnPointIndexs.Count)];
+            
+            print($"randomVacantAppleSpawnPoint = {randomVacantAppleSpawnPoint}");
+            Instantiate(applePrefab, appleSpawnPoints[randomVacantAppleSpawnPoint].SpawnLocation, Quaternion.identity);
+        }
+
+        private List<int> CheckVacentSpawnPoints()
+        {
+            List<int> spawnPointIndexs = new List<int>();
+            for(int i = 0; i < appleSpawnPoints.Length; i++)
+            {
+                if(!appleSpawnPoints[i].appleAtLocation)
+                {
+                    spawnPointIndexs.Add(i);
+                }
+            }
+
+            return spawnPointIndexs;
+        }
+
+        private void Check_maxNumberOfApples_IsntTooLarge() 
+        {
+            if(appleSpawnPoints != null && maxNumberOfApples > appleSpawnPoints.Length)
+                maxNumberOfApples = appleSpawnPoints.Length;
+        }
+
+        private void Refresh_appleSpawnPoints() => appleSpawnPoints = FindObjectsOfType(typeof(AppleSpawnPoint)) as AppleSpawnPoint[];
+
     }
 }
